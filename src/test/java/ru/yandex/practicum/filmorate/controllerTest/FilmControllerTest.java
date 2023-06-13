a36f28d8ab4dc3bf3c7de5fb61e0a3ddcffb9e30
Nikolai <gardemarinin5@yandex.ru>
2023-06-13 18:10:59 +0500

    ТЗ10:checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/controllerTest/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllerTest/FilmControllerTest.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
-import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
-import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.time.LocalDate;
 
